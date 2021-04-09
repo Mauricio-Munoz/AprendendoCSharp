fafcff07262d5bb98b85e571ccc97c7c60a0c730
Mauricio Muñoz <mdmunoz@supportse.com.br>
2021-04-09 10:00:39 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trabalhando com váriaveis do Tipo Double");
+
+            double salario = 2100.57;
+                      
+
+            Console.WriteLine(salario);
+
+            Console.ReadLine();
+        }
+    }
+}
